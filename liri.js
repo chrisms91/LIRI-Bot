150c7c70f2399c7e1ddacff7ca2c384e1106fd2f
Christopher Kim <christopherkim@dhcp-v213-090.vp.reshsg.uci.edu>
2017-08-18 02:25:38 -0700

    initialized api

--- liri.js
+++ liri.js
@@ -1,4 +1,22 @@
+// Load keys
 var KEYS = require('./keys.js');
 
-console.log(KEYS.omdbKey.api_key)
-console.log(KEYS);
+// Initialize spotify api
+// https://www.npmjs.com/package/node-spotify-api
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: KEYS.spotifyKeys.id,
+  secret: KEYS.spotifyKeys.secret
+});
+
+// Initialize twitter api
+// https://www.npmjs.com/package/twitter
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: KEYS.twitterKeys.consumer_key,
+  consumer_secret: KEYS.twitterKeys.consumer_secret,
+  access_token_key: KEYS.twitterKeys.access_token_key,
+  access_token_secret: KEYS.twitterKeys.access_token_secret
+});
