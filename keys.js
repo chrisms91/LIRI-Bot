a79bfe4e018c62327aa2dfa659bd824d18272cf6
Christopher Kim <christopherkim@dhcp-v213-090.vp.reshsg.uci.edu>
2017-08-18 00:03:57 -0700

    deleted keys from keys.js

--- keys.js
+++ keys.js
@@ -1,10 +1,3 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: 'yjw6r8LwrMghavx3zlQWqH66S',
-  consumer_secret: 'avbdQr6mFRN2RA4Ak8eBiKbxsAMUIIGNprFAEzhNTKPkONLUcU',
-  access_token_key: '898433456530575360-pDNVY3ms9C6D1bFuLIJdqfyq4v0MHS9',
-  access_token_secret: 'NushhbJg1lUrcEivhgV9gcBnS269jq4ss5tsPlUUB83FF'
-}
-
-console.log(process.env.OMDB_API_KEY);
+console.log(process.env);
