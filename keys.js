e98d420ec160d3a017f6b2963818e10a89f49681
Christopher Kim <christopherkim@dhcp-v213-090.vp.reshsg.uci.edu>
2017-08-18 00:40:22 -0700

    keys are public for now

--- keys.js
+++ keys.js
@@ -1,3 +1,12 @@
-console.log('this is loaded');
+console.log('KEYS are loaded');
 
-console.log(process.env);
+exports.twitterKeys = {
+  consumer_key: 'yjw6r8LwrMghavx3zlQWqH66S',
+  consumer_secret: 'avbdQr6mFRN2RA4Ak8eBiKbxsAMUIIGNprFAEzhNTKPkONLUcU',
+  access_token_key: '898433456530575360-pDNVY3ms9C6D1bFuLIJdqfyq4v0MHS9',
+  access_token_secret: 'NushhbJg1lUrcEivhgV9gcBnS269jq4ss5tsPlUUB83FF'
+}
+
+exports.omdbKey = {
+	api_key: '40e9cece'
+}
